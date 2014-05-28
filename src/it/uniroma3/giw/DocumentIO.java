0dcac8d07e841a004b01e46007ec004f61701cca
m4rc090 <marco.coluzza@gmail.com>
2014-05-28 11:23:29 +0200

    Aggiornato documentIO

--- src/it/uniroma3/giw/DocumentIO.java
+++ src/it/uniroma3/giw/DocumentIO.java
@@ -1,13 +1,17 @@
 package it.uniroma3.giw;
 
 import java.io.File;
-import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class DocumentIO {
 	private String id2urlSport;
+	private String id2urlMovies;
+	private String id2urlGames;
+	private String id2urlNews;
+	
+
 	private String csvpath;
 	
 	public DocumentIO(){
@@ -23,6 +27,13 @@ public class DocumentIO {
 		}		
 
 		this.setId2urlSport(conf.getProperty("id2urlSport-path"));
+		
+		this.setId2urlGames(conf.getProperty("id2urlGames-path"));
+		
+		this.setId2urlMovies(conf.getProperty("id2urlMovie-path"));
+		
+		this.setId2urlNews(conf.getProperty("id2urlNews-path"));
+		
 		this.setCsvpath(conf.getProperty("csv-path"));
 	}
 
@@ -40,6 +51,30 @@ public class DocumentIO {
 
 	public void setCsvpath(String csvpath) {
 	    this.csvpath = csvpath;
+	}
+	
+	public String getId2urlMovies() {
+		return id2urlMovies;
+	}
+
+	public void setId2urlMovies(String id2urlMovies) {
+		this.id2urlMovies = id2urlMovies;
+	}
+
+	public String getId2urlGames() {
+		return id2urlGames;
+	}
+
+	public void setId2urlGames(String id2urlGames) {
+		this.id2urlGames = id2urlGames;
+	}
+
+	public String getId2urlNews() {
+		return id2urlNews;
+	}
+
+	public void setId2urlNews(String id2urlNews) {
+		this.id2urlNews = id2urlNews;
 	}
 	
 	public void cleanPath() {
